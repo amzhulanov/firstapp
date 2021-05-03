4ec87a1074ecc24e6ace939cf75c8c8410407ad3
amzhulanov <amzhulanov@ya.ru>
2021-05-04 00:56:38 +0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const obj = {
+    x: 1,
+};
+
+const A = () =>{
+<h1>Hello app</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
+
+myClass
+
